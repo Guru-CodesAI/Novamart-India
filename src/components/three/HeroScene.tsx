78d1b85c001062e2087b33cc83820175674f666d
GURUNATHAN <vengaigurunathan2gmail.com>
2026-06-23 15:14:51 +0530

    fix bufferAttribute constructor args in HeroScene

--- src/components/three/HeroScene.tsx
+++ src/components/three/HeroScene.tsx
@@ -71,9 +71,7 @@ function ParticleField() {
       <bufferGeometry>
         <bufferAttribute
           attach="attributes-position"
-          count={count}
-          array={positions}
-          itemSize={3}
+          args={[positions, 3]}
         />
       </bufferGeometry>
       <pointsMaterial
